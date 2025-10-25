405a05c6d82764b8e57076c71438b98d7ba05cd2
LuisGon23 <gonzalezmluis08@gmail.com>
2025-10-25 00:26:30 -0500

    02_escenario_interactivo
    Avance de aplicación java

--- 2_escenario_interactivo/index.js
+++ 2_escenario_interactivo/index.js
@@ -1,14 +1,86 @@
-const jell = document.querySelectorAll('.jellyfish');
-const tablero = document.querySelector('.contador');
+
+const jell = document.querySelectorAll('.escenario-dragonball .jellyfish');
+const tablero = document.querySelector('.escenario-dragonball .contador h2');
 let counter = 0;
 
 console.log(jell, tablero);
 
 jell.forEach(item => {
-    item.addEventListener('click', () => {    
-        item.style.filter = "grayscale(1)";
-        item.classList.add('salto');
-        counter++;
-        tablero.textContent = counter;
+    item.addEventListener('click', () => {
+        if (item.style.filter !== "grayscale(1)") {
+            item.style.filter = "grayscale(1)";
+            item.classList.add('salto');
+            counter++;
+            tablero.textContent = counter;
+        }
     });
+});
+
+const ataques = document.querySelectorAll('.namek .mover');
+const freezer = document.querySelector('.namek .freezer');
+const vidaFreezerDisplay = document.querySelector('.namek .contador h2');
+const tituloContadorNamek = document.querySelector('.namek .contador h1');
+
+let vidaFreezer = 100; 
+vidaFreezerDisplay.textContent = vidaFreezer;
+
+ataques.forEach(ataque => {
+    ataque.addEventListener('click', () => {
+        if (vidaFreezer <= 0) {
+            return;
+        }
+
+        ataque.style.display = 'none';
+
+        
+        vidaFreezer -= 20;
+
+        if (vidaFreezer < 0) {
+            vidaFreezer = 0;
+        }
+
+        vidaFreezerDisplay.textContent = vidaFreezer;
+
+        freezer.classList.add('freezer-hit');
+        setTimeout(() => {
+            freezer.classList.remove('freezer-hit');
+        }, 300); 
+
+      
+        if (vidaFreezer <= 0) {
+            tituloContadorNamek.textContent = "¡Freezer derrotado!";
+            freezer.classList.add('freezer-derrotado');
+        }
+    });
+});
+
+
+const gokuBlue = document.querySelector('.gokublue');
+const gokuBlueImg = document.querySelector('.gokublue img');
+const aura = document.querySelector('.aura');
+const contadorGoku = document.querySelector('.planetabils .contador h2');
+
+let clicsGoku = 0;
+const clicsParaTransformar = 5;
+
+gokuBlue.addEventListener('click', () => {
+    
+    if (clicsGoku >= clicsParaTransformar) {
+        return;
+    }
+
+    
+    clicsGoku++;
+
+   
+    contadorGoku.textContent = clicsGoku;
+
+    aura.classList.add('activada');
+    setTimeout(() => aura.classList.remove('activada'), 500); 
+
+    if (clicsGoku === clicsParaTransformar) {
+        contadorGoku.textContent = "¡UI!";
+        gokuBlueImg.src = 'pngwing.com (2).png';
+        aura.classList.add('aura-ui', 'activada');
+    }
 });
