8ec238546ae05d710099a359840d822e1df72dd2
LuisGon23 <gonzalezmluis08@gmail.com>
2025-11-09 19:57:24 -0500

    Base_Datos_Creada
    Avance

--- 03_Crear_bases_datos_comic/bd.js
+++ 03_Crear_bases_datos_comic/bd.js
@@ -2,7 +2,7 @@ let comic = {
   "nombreComic": "Eco olvidado",
   "numeroCapitulos": "3",
   "genero": "....",
-  "sipnosis": "...",
+  "sipnosis": "Un joven de 17 años decide ir a un museo abandonado donde encontrar un peculiar mentor del pasado",
   "autores": ["Santiago", "Luis", "Salome"],
   "portadaComic": "imagen.jpg",
   "year": 2025,
@@ -10,24 +10,25 @@ let comic = {
     {
         "id": 1,
       "nombre": "Capítulo 1",
-     "personajes": ["..", "...", "...."],
+     "personajes": ["Raizura", "Abuela", "Reportero", "Constructores"],
      "portada": "imagen1.jpg",
      "descripcion": "..."
     },
     {
    "id": 2,
       "nombre": "Capítulo 2",
-     "personajes": ["..", "...", "...."],
+     "personajes": ["Raizura", "Fruko"],
      "portada": "imagen1.jpg",
-     "descripcion": "..."
+     "descripcion": "Conocemos a raizura, un joven lleno de curiosidad y fascinacion por la musica decide investigar un museo de musica"
       
     },
     {
       "id": 3,
       "nombre": "Capítulo 3",
-     "personajes": ["..", "...", "...."],
+     "personajes": ["Raizura", "Fruko", "Abuela", "Trabajadores", "Ciudadanos"],
      "portada": "imagen1.jpg",
-     "descripcion": "..."
+     "descripcion":  "Raizura decide hacer un concierto para evitar una tragedia",
+
     }
   ]
 }
